75e21797513a38628a247db7724ed9e381226043
KakaVin <37044283+KakaVin@users.noreply.github.com>
2018-10-20 16:57:57 +0500

    Add files via upload

--- gitHub.pl
+++ gitHub.pl
@@ -5,6 +5,7 @@ use JSON::PP;
 my $user = "kraih";
 my $rangeDate = localtime->add_years(-1);
 my @nameRepos;
+my $marking;
 my $getJson = get("https://api.github.com/users/$user/repos") or die;
 my $json = decode_json( $getJson );
 for (my $i = 0; $i != 100; $i++){
@@ -12,21 +13,20 @@ for (my $i = 0; $i != 100; $i++){
 		@nameRepos[$i] = $json->[$i]{'name'};
 		} else { last }
 }
-my $marking;
 say "User \"$user\" repositories: ", scalar @nameRepos;
 say "Date ", $rangeDate->ymd,"  -  ",localtime->ymd,"\n";
-foreach my $i( 0..$#nameRepos ){
-	my @dateCommit = ();
+for my $i( 0..$#nameRepos ){
+	my @dateCommit;
 	$getJson = get( "https://api.github.com/repos/$user/@nameRepos[$i]/commits" );
 	$json = decode_json( $getJson );
 	for (my $i = 0; $i != 100; $i++){
-		if ($json->[$i]{'commit'}{'author'}{'date'} ne ''){
+		if ( $json->[$i]{'commit'}{'author'}{'date'} ne '' ){
 			@dateCommit[$i] = $json->[$i]{'commit'}{'author'}{'date'};
 		} else { last }
 	}
 	$marking = 0;
-	ShowRepos(@nameRepos[$i], @dateCommit);
-	if ($marking == 0){say "no commits last year\n"}
+	ShowRepos (@nameRepos[$i], @dateCommit);
+	if ( $marking == 0 ){ say "no commits last year\n" }
 }
 sub ShowRepos{
 	say "Name:   ", @_[0];
@@ -34,7 +34,7 @@ sub ShowRepos{
 	for (my $i = 1; $i < scalar @_; $i++){
 		my $datePiece = Time::Piece->strptime( @_[$i], '%Y-%m-%dT%H:%M:%SZ' );
 		if ( $datePiece >= $rangeDate ){ 
-			push(@datePiece, $datePiece);
+			push @datePiece, $datePiece;
 			if ( $marking == 0 ){ 
 				$marking = 1;
 				print "   ";
@@ -48,14 +48,14 @@ sub ShowRepos{
 			}
 		}
 	}
-	foreach my $i(1..7){ getLine($i, @datePiece) }
+	for my $i(1..7){ PrintLine($i, @datePiece) }
 	if ( $marking == 1 ){ say; }
 }	
-sub getLine{
+sub PrintLine{
 	my @hour = (0) x 24;
 	my @day;
 	for (my $i = 1; $i < scalar @_; $i++){
-		if (@_[$i]->wday == @_[0]){ 
+		if ( @_[$i]->wday == @_[0] ){ 
 			@hour[ ( @_[$i]->hour ) ]++;
 			@day = @_[$i]->day;
 		} 
@@ -63,15 +63,15 @@ sub getLine{
 	my $returLine = "";
 	my $print = 0;
 	for (my $i = 0; $i < scalar @hour; $i++){
-		if (@hour[$i] == 0 ){ $returLine = $returLine."   " }
-		if (@hour[$i] >10 ){
-			$returLine = $returLine." @hour[$i]";
-			$print++
+		if ( @hour[$i] ){ $returLine = $returLine."   " }
+		if ( @hour[$i] >= 10 ){
+			$returLine .= " @hour[$i]";
+			$print++;
 		} 
-		if (@hour[$i] <10 and @hour[$i] >0 ){
-			$returLine = $returLine."  @hour[$i]";
-			$print++
+		if ( @hour[$i] < 10 and @hour[$i] > 0 ){
+			$returLine .= "  @hour[$i]";
+			$print++;
 		} 	
 	}
-	if ($print){print @day, $returLine, "\n"}
+	if ( $print ){ print @day, $returLine, "\n" }
 }
