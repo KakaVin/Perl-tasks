8fc372c2d46aba7193dc12e5167a59ce994249c7
KakaVin <37044283+KakaVin@users.noreply.github.com>
2018-10-25 18:41:39 +0500

    Add files via upload

--- parseEmail.pl
+++ parseEmail.pl
@@ -1,69 +1,94 @@
 use 5.016;
 use Test::More;
-my %return;
 
-parse_email('mail@example.com');
-is $return{'Mail'}, 'mail', 'mail correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
+diag("\ncorrect\n");
+my %parse_email = parse_email('mail@example.com');
+is $parse_email{'Mail'}, 'mail', 'mail correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('Name <mail@example.com>');
+is $parse_email{'Name'}, 'Name', 'name correct';
+is $parse_email{'Mail'}, 'mail', 'mail correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('"Ather Name" <mail@example.com>');
+is $parse_email{'Name'}, 'Ather Name', 'name correct';
+is $parse_email{'Mail'}, 'mail', 'mail correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('mail+ext@example.com');
+is $parse_email{'Mail'}, 'mail+ext', 'mail correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('Name <mail+ext@example.com>');
+is $parse_email{'Name'}, 'Name', 'name correct';
+is $parse_email{'Mail'}, 'mail+ext', 'mail correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('"login"@example.com');
+is $parse_email{'Login'}, 'login', 'login correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('"l o g i n"@example.com');
+is $parse_email{'Login'}, 'l o g i n', 'login correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('Name "login"@example.com');
+is $parse_email{'Name'}, 'Name', 'name correct';
+is $parse_email{'Login'}, 'login', 'login correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
+%parse_email = parse_email('"Ather Name" "l o g i n"@example.com');
+is $parse_email{'Name'}, 'Ather Name', 'name correct';
+is $parse_email{'Login'}, 'l o g i n', 'login correct';
+is $parse_email{'Domain'}, 'example.com', 'domain correct';
 
-parse_email('Name <mail@example.com>');
-is $return{'Name'}, 'Name', 'name correct';
-is $return{'Mail'}, 'mail', 'mail correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
-parse_email('"Ather Name" <mail@example.com>');
-is $return{'Name'}, 'Ather Name', 'name correct';
-is $return{'Mail'}, 'mail', 'mail correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
-parse_email('mail+ext@example.com');
-is $return{'Mail'}, 'mail+ext', 'mail correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
-parse_email('Name <mail+ext@example.com>');
-is $return{'Name'}, 'Name', 'name correct';
-is $return{'Mail'}, 'mail+ext', 'mail correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
-parse_email('"login"@example.com');
-is $return{'Login'}, 'login', 'login correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
-parse_email('"l o g i n"@example.com');
-is $return{'Login'}, 'l o g i n', 'login correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
-parse_email('Name "login"@example.com');
-is $return{'Name'}, 'Name', 'name correct';
-is $return{'Login'}, 'login', 'login correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
-parse_email('"Ather Name" "l o g i n"@example.com');
-is $return{'Name'}, 'Ather Name', 'name correct';
-is $return{'Login'}, 'l o g i n', 'login correct';
-is $return{'Domain'}, 'example.com', 'domain correct';
-
+diag("\nnot correct\n");
+%parse_email = parse_email('Name <mail@example.com');
+is $parse_email{'Name'}, undef, 'name undef';
+is $parse_email{'Mail'}, undef, 'mail undef';
+is $parse_email{'Domain'}, undef, 'domain undef';
+%parse_email = parse_email('"mail@example.com');
+is $parse_email{'Mail'}, undef, 'mail undef';
+is $parse_email{'Domain'}, undef, 'domain undef';
+%parse_email = parse_email('ma"il@example.com');
+is $parse_email{'Mail'}, undef, 'mail undef';
+is $parse_email{'Domain'}, undef, 'domain undef';
+%parse_email = parse_email('Name mail@example.com>');
+is $parse_email{'Name'}, undef, 'name undef';
+is $parse_email{'Mail'}, undef, 'mail undef';
+is $parse_email{'Domain'}, undef, 'domain undef';
+%parse_email = parse_email('"l o g i n"@example com');
+is $parse_email{'Name'}, undef, 'name undef';
+is $parse_email{'Login'}, undef, 'login undef';
+is $parse_email{'Domain'}, undef, 'domain undef';
+%parse_email = parse_email('mail+ext+undef@example.com');
+is $parse_email{'Mail'}, undef, 'mail undef';
+is $parse_email{'Domain'}, undef, 'domain undef';
+%parse_email = parse_email('mail@example.com.ru');
+is $parse_email{'Mail'}, undef, 'mail undef';
+is $parse_email{'Domain'}, undef, 'domain undef';
 done_testing();
 
-sub parse_email{
+sub parse_email($){
 	say my $mail = @_[0];
-$mail =~ m{
-( 	( "(?'name'\N+)" \s<* ( "(?'login'\N+)" )* )  
+	my %return;
+$mail =~ m{^
+( 	( "(?'name'\N+)" \s ( "(?'login'\N+)" )* )  
 	| 
-	( (?'name'\w+)\s<* )
+	( (?'name'\w+)\s )
 	| 
-	(\W*)
+	(\s*)
 )
 (
-	(?'mail' (\w+) | (\w+\+\w+) ) 
-	| 
-	"(?'login'\N+)"
-)
-	@
-	(?'domain'\w+.\w+)	
+	<(?'mail' (\w+) | (\w+\+\w+) ) @ (?'domain'\w+\.\w+)>
+	|
+	(
+		(
+			(?'mail' (\w+) | (\w+\+\w+) )
+			|
+			"(?'login'\N+)"
+		)
+		@ (?'domain'\w+\.\w+)
+	)				
+) $
 }x;
 $return{'Name'} = $+{name};
 $return{'Mail'} = $+{mail};
 $return{'Login'} = $+{login};
 $return{'Domain'} = $+{domain};
+
+return %return;
 }
