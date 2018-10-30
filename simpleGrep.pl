86def374cf0364f62097e1bdce3c26b0c6927fcc
KakaVin <37044283+KakaVin@users.noreply.github.com>
2018-10-30 22:48:44 +0500

    Add files via upload

--- simpleGrep.pl
+++ simpleGrep.pl
@@ -1,28 +1,29 @@
 use 5.016;
+use Getopt::Std;
 
-my $catalog = "/home/alex/Рабочий стол";
-my $pattern = "sub";
-
-show_dir($catalog);
+getopts("p:c:");
+our($opt_p, $opt_c);
+my $pattern = $opt_p;
+show_dir($opt_c);
 
 sub show_dir($){
 my $dir = $_[0];
 opendir(my $d, "$dir") or die "$dir $!";
 for ( readdir($d) ) { 
 	next if $_=~ /\.\.?$/;
-	if (-T "$dir/$_") { 
+	if (-T "$dir/$_" && -R "$dir/$_") { 
 		open (InFile, "$dir/$_") or die $!;
 		my $num_line = 0;
 		while ( my $line = <InFile> )
 		{
 			$num_line++;
-			print "$dir/$_:$num_line:# $line" if $line =~ /$pattern/
+			print "$dir/$_:$num_line:# $line" if $line =~ /$pattern/i;
 		}
 		close (InFile);
 	} 
-	if (-d "$dir/$_") {
+	if (-d "$dir/$_" && -R "$dir/$_") {
 		show_dir("$dir/$_");
 	};
 };
-#closedir($d) or warn $!;
+closedir($d) or warn $!;
 }
